eeef1f945b26ddc7c9109b5065eb526ef8dc388f
Wissam Noureddine <wissam62500@gmail.com>
2020-11-29 16:43:39 +0200

    New features for the Zoom Call Analyzer

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Network Application Analyzers
 
 ## Whatsapp Call Analyzer
+
+The CLI Tool could be used to:
 - Detect the start and end of a call
 - Detect the duration of a call
+- Dected which user ended the call
+
+### Samples generated using the tool
 
 ## Zoom Call Analyzer
+
+The CLI Tool could be used to:
+- Detect the start and end of a zoom meeting
+- Detect when the user is in the waiting room or not yet connected to the meeting
+- Dectect the duration of a zoom meeting
+
+### Samples generated using the tool
--- zoomAnalyzer.py
+++ zoomAnalyzer.py
@@ -10,12 +10,41 @@ def UsedProtocols(protocols):
             listOfProtocolNames.append(p)
     return listOfProtocolNames
 
-def plotPacketByProtocol(protocolName, lineColor, lineWidth, samplingRate,cap):
-    protocolPackets = list(map(lambda p: 1 if protocolName in p else 0, ListOfProtocols))
-    protocolPackets, timestamps = samplePackets(protocolPackets,ListOfTimeStamps,cap, samplingRate)
-    plt.plot(timestamps, protocolPackets, color=lineColor, label=protocolName, linewidth=lineWidth)
-    return timestamps
 
+#Client Key Exchange, Change Cipher Spec, Encrypted Handshake Message
+def detectWhenTheMeetingWasStrated():
+    timestamps = []
+    SuccessRequestInfoPackets = list(map(lambda  i: 1 if "Client Key Exchange, Change Cipher Spec, Encrypted Handshake Message" in str(i) else 0, ListOfProtocolInformation))
+    UDPPackets = list(map(lambda  p: 1 if "UDP" in p else 0, ListOfProtocols))
+    for i in range(len(SuccessRequestInfoPackets)):
+        if SuccessRequestInfoPackets[i] == 1:
+            timestamps.append(ListOfTimeStamps[i-4])
+    count = 0
+    startTime = 0
+    lastUDPPacketTimeStamp = 0
+    for t in timestamps:
+        if count == 0:
+            plt.plot(t,0,'go')
+            plt.axvline(t, 0, 100, label='Zoom Call Started', color = "red", linewidth=2)
+            startTime = t
+        
+        count+=1
+    count = 0
+    for t in ListOfTimeStamps:
+        if UDPPackets[count] == 0:
+            if(lastUDPPacketTimeStamp == 0):
+                lastUDPPacketTimeStamp = t
+        elif UDPPackets[count] == 1:
+            lastUDPPacketTimeStamp = 0
+        count+=1
+    print("Meeting Started at: "+ str(startTime)+" seconds")
+    
+    if(lastUDPPacketTimeStamp > 0):
+        plt.axvline(lastUDPPacketTimeStamp, 0, 100, label='Zoom Call Ended', color = "cyan", linewidth=2)
+        print("Meeting Ended at: "+ str(lastUDPPacketTimeStamp)+" seconds")
+        print("The User entered the meeting for %.2f seconds"%((lastUDPPacketTimeStamp - startTime)))
+    else:
+        print("End of meeting not detected!")
 
 def samplePackets(packets,timestamps, cap, samplingRate):
     returnedPackets = []
@@ -40,30 +69,57 @@ def samplePackets(packets,timestamps, cap, samplingRate):
     return returnedPackets, returnedTimeStamps
 
 SAMPLING_RATE = 0.02
-SAMPLING_CAP = 2000
-
+SAMPLING_CAP = 1400
 # reading csv file
-data = pd.read_csv("data/zoomCall2.csv", sep=",", encoding="ISO-8859-1")
+data = pd.read_csv("data/zoomCall6.2.csv", sep=",", encoding="ISO-8859-1")
 
 # extracting the Time, Protocol and Info column values
 ListOfTimeStamps = data.Time.values
 ListOfProtocols = data.Protocol.values
+ListOfPacketsLengths = data.Length.values
 ListOfProtocolInformation = data.Info.values
 
-# plotting packs
-timestamps = plotPacketByProtocol(protocolName="TCP",lineColor="blue",lineWidth=1, cap=SAMPLING_CAP, samplingRate= SAMPLING_RATE)
-plotPacketByProtocol(protocolName="UDP",lineColor="green",lineWidth=1, cap=SAMPLING_CAP, samplingRate= SAMPLING_RATE)
-plotPacketByProtocol(protocolName="WireGuard",lineColor="red",lineWidth=1, cap=SAMPLING_CAP, samplingRate= SAMPLING_RATE)
-#plotPacketByProtocol(protocolName="DNS",lineColor="olive",lineWidth=1, cap=SAMPLING_CAP, samplingRate= SAMPLING_RATE)
-#plotPacketByProtocol(protocolName="ARC",lineColor="pink",lineWidth=2, cap=SAMPLING_CAP, samplingRate= SAMPLING_RATE)
-# printing used protocols
-print(UsedProtocols(ListOfProtocols))
 
-plt.ylabel('Number of packets')
-plt.xlabel('Time in seconds')
-plt.legend(loc="upper right")
-plt.title('Packets recorded during a Zoom call')
-plt.margins(0,0.05,tight=True)
-plt.xticks(np.arange(0, int(timestamps[len(timestamps)-1]), 5))
-plt.ylim(bottom=0)
-plt.show()
+def plotGraphByNumberOfPacketsVersusTime(protocols, plotColors):
+
+    # plotting packs
+    for i in range(len(protocols)):
+        protocolPackets = list(map(lambda p: 1 if protocols[i] in p else 0, ListOfProtocols))
+        protocolPackets, timestamps = samplePackets(protocolPackets,ListOfTimeStamps,SAMPLING_CAP, SAMPLING_RATE)
+        plt.plot(timestamps, protocolPackets, color=plotColors[i], label=protocols[i], linewidth=1)
+    detectWhenTheMeetingWasStrated()
+    plt.ylabel('Number of packets')
+    plt.xlabel('Time in seconds')
+    plt.legend(loc="upper right")
+    plt.title('Traffic recorded during a Zoom call')
+    plt.margins(0,0.05,tight=True)
+    plt.xticks(np.arange(0, int(timestamps[len(timestamps)-1]), 15))
+    plt.ylim(bottom=0)
+    plt.show()
+
+def plotGraphByLengthOfPacketsVersusTime(protocols, plotColors):
+    # plotting packs
+    for i in range(len(protocols)):
+        plt.plot(ListOfTimeStamps, ListOfPacketsLengths, color=plotColors[i], label=protocols[i], linewidth=0.09)
+
+    detectWhenTheMeetingWasStrated()
+    plt.ylabel('Length of packets')
+    plt.xlabel('Time in seconds')
+    plt.legend(loc="upper right")
+    plt.suptitle('Traffic recorded during a Zoom call')
+    plt.margins(0,0.05,tight=True)
+    plt.xticks(np.arange(0, int(ListOfTimeStamps[len(ListOfTimeStamps)-1]), 15))
+    plt.ylim(bottom=0)
+    
+    plt.show()
+
+# plot TCP and UDP packets
+
+# plotGraphByLengthOfPacketsVersusTime(protocols=["TCP","UDP"],plotColors=["blue","green"])
+# plotGraphByNumberOfPacketsVersusTime(protocols=["TCP","UDP"],plotColors=["blue","green"])
+
+
+# plot UDP Packets 
+
+#plotGraphByLengthOfPacketsVersusTime(protocols=["UDP"],plotColors=["blue"])
+# plotGraphByNumberOfPacketsVersusTime(protocols=["UDP"],plotColors=["blue"])
